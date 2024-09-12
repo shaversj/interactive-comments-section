853fda9219f6c3b8a9c7729b9b2624a513472595
Josh Shavers <shaversj@gmail.com>
2024-09-12 08:03:12 -0500

    Add basic structure of Comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="">
+      <div>
+        {/*Comment Card*/}
+        <div>{/*  Vote */}</div>
+        <div>
+          <div>{/*  Comment Heading */}</div>
+          <div>{/*  Comment Body */}</div>
+        </div>
+      </div>
     </div>
   );
 }
