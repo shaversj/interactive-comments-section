0a894c16c47a0e3bd6d6d8d78db9e5cab6851f03
Josh Shavers <shaversj@gmail.com>
2024-09-12 08:21:19 -0500

    Add vote elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 import Image from "next/image";
+import Plus from "@/components/icons/Plus";
+import Minus from "@/components/icons/Minus";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="grid place-items-center">
       <div>
         {/*Comment Card*/}
-        <div>{/*  Vote */}</div>
+        <div className={"bg-very-light-gray flex h-[6.25rem] w-10 flex-col items-center justify-between rounded-lg py-[11.9px]"}>
+          {/*  Vote */}
+          <Plus />
+          <span className={"text-moderate-blue font-bold"}>12</span>
+          <Minus />
+        </div>
         <div>
           <div>{/*  Comment Heading */}</div>
           <div>{/*  Comment Body */}</div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,25 +1,22 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
-        colors: {
-          'moderate-blue': 'hsl(238, 40%, 52%)',
-          'soft-red': 'hsl(358, 79%, 66%)',
-          'light-grayish-blue': 'hsl(239, 57%, 85%)',
-          'pale-red': 'hsl(357, 100%, 86%)',
-          'dark-blue': 'hsl(212, 24%, 26%)',
-          'grayish-blue': 'hsl(211, 10%, 45%)',
-          'light-gray': 'hsl(223, 19%, 93%)',
-          'very-light-gray': 'hsl(228, 33%, 97%)',
-          'white': 'hsl(0, 0%, 100%)',
-        },
+        "moderate-blue": "hsl(238, 40%, 52%)",
+        "soft-red": "hsl(358, 79%, 66%)",
+        "light-grayish-blue": "hsl(239, 57%, 85%)",
+        "pale-red": "hsl(357, 100%, 86%)",
+        "dark-blue": "hsl(212, 24%, 26%)",
+        "grayish-blue": "hsl(211, 10%, 45%)",
+        "light-gray": "hsl(223, 19%, 93%)",
+        "very-light-gray": "hsl(228, 33%, 97%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+      fontFamily: {
+        rubik: ["var(--font-rubik)"],
       },
     },
   },
